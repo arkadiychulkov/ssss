3e016b8f75e5f574f0661e8f5fd265f7eecb38b6
user <arkadiychulkov11@gmail.com>
2023-01-28 14:07:18 +0200

    ssssss

--- .idea/main.py
+++ .idea/main.py
@@ -2,16 +2,13 @@ class Students:
     group = 2929
     amout_of_student = 0;
     def __init__(self, heigh = 160):
-        print(id(self))
         self.heigh = heigh
         Students.amout_of_student += 1
+    def grow (self, heigh = 1):
+        self.heigh += heigh
+
 
 st1 = Students()
-print(st1.heigh)
+st1.grow(heigh=25)
 st2 = Students()
-print(st2.heigh)
-print(Students.amout_of_student)
-print(st1.group)
-print(st2.group)
-
-st1.group = 2323
+print(st1.heigh)
